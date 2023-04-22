54126b3452016ce17c5908ce6f7b98d7874fa62b
ingv <ingvarr.vargardd@gmail.com>
2023-04-22 10:18:33 +0300

    add support for formulas (wip)

--- Choe/Base/LiteralEvaluator.cs
+++ Choe/Base/LiteralEvaluator.cs
@@ -1,18 +1,18 @@
 ﻿namespace Choe
 {
-  public class LiteralEvaluator
-  {
-    protected LiteralIs isfunction;
-    protected LiteralEvaluate evaluationfunction;
-
-    public LiteralEvaluator(LiteralIs li, LiteralEvaluate le)
+    public class LiteralEvaluator
     {
-      this.isfunction = li;
-      this.evaluationfunction = le;
+        protected LiteralIs isfunction;
+        protected LiteralEvaluate evaluationfunction;
+
+        public LiteralEvaluator(LiteralIs li, LiteralEvaluate le)
+        {
+            this.isfunction = li;
+            this.evaluationfunction = le;
+        }
+
+        public LiteralIs Is => this.isfunction;
+
+        public LiteralEvaluate Evaluate => this.evaluationfunction;
     }
-
-    public LiteralIs Is => this.isfunction;
-
-    public LiteralEvaluate Evaluate => this.evaluationfunction;
-  }
 }
