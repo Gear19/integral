b34e3f5d41ae5e5442532b2166941ae439a36eb5
Рома Ибрагимов <www.roma.ibr@gmail.com>
2023-05-19 21:52:00 +0300

    DA

--- Choe/Program.cs
+++ Choe/Program.cs
@@ -16,7 +16,7 @@ namespace Choe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new InputForm());
             
         }
     }
